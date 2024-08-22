b88a060932cfcaafa5d219453c4513e69541344e
Ygorhenrique <118612433+YgorHenriqueLima@users.noreply.github.com>
2024-08-22 20:06:39 -0300

    terminando exercicio

--- Praticas-intermediarias/Gerador_de_bolinhas/script.js
+++ Praticas-intermediarias/Gerador_de_bolinhas/script.js
@@ -1,5 +1,23 @@
 // CÓDIGO ESTUDADO NO CANAL DO CFB CURSOS
 
+// Class Bola
+/*
+    quando adicionamos uma bola ela tem atributos em comum
+    -> cor diferente
+    -> velocidade diferente
+    -> direções diferentes
+    -> tamanhos diferentes
+*/ 
+
+
+ // cada bolinha que instanciar na tela tem que ter um id diferente, pois cada bolinha será controlado por um objeto Bola instanciada no array Bolas, tenho que saber
+        /*
+            -> qual meu ID;
+            -> qual meu palco;
+            -> onde instanciar;
+            
+        */
+
 const palco = document.getElementById('palco')
 const num_objeto  = document.getElementById('num_objetos')
 const txt_qtde  = document.getElementById('txt_qtde')
@@ -12,24 +30,7 @@ let alturaPalco = palco.offsetHeight
 let bolas = []
 let numBolas = 0
 
-// Class Bola
-/*
-    quando adicionamos uma bola ela tem atributos em comum
-    -> cor diferente
-    -> velocidade diferente
-    -> direções diferentes
-    -> tamanhos diferentes
-*/ 
-
-class Bola { 
-    
-        // cada bolinha que instanciar na tela tem que ter um id diferente, pois cada bolinha será controlado por um objeto Bola instanciada no array Bolas, tenho que saber
-        /*
-            -> qual meu ID;
-            -> qual meu palco;
-            -> onde instanciar;
-            
-        */
+class Bola {
     constructor(arrayBolas, palco){
         //vamos definir o tamanho da bolinha, vamos gerar os tamanhos automaticamente com o gerador random da classe Math,e o floor permite o arredondamento
         this.tam = Math.floor(Math.random()*15)+10
@@ -44,41 +45,90 @@ class Bola {
         */
         this.px = Math.floor(Math.random()*(larguraPalco-this.tam))
         this.py = Math.floor(Math.random()*(alturaPalco-this.tam))
-
         this.velx = Math.floor(Math.random()*2)+0.5
         this.vely = Math.floor(Math.random()*2)+0.5
-        /*definindo as direções das bolinhas*/
         this.dirx = (Math.random()*10) > 5 ? 1 : -1
         this.diry = (Math.random()*10) > 5 ? 1 : -1
-
         this.palco = palco
         this.arrayBolas = arrayBolas
         this.id = Date.now()+"_"+Math.floor(Math.random*10000000000)
-       
         this.desenhar()
-
         this.controle=setInterval(this.controlar, 10)
         this.eu=document.getElementById(this.id)
+        num_objeto.innerHTML = numBolas 
     }
 
     // função para retornar a posição da bolinha
 
     minhaPos=()=>{
-
+        return this.arrayBolas.indexOf(this)
     }
 
     remover=()=>{
-
-    }
-
-    // método para controlar a movimentação das bolinhas
-    controlar=()=>{
-
+        clearInterval(this.controle)
+        bolas = bolas.filter((b)=>{
+            if(b.id != this.id){
+                return b
+            }
+        })
+        this.eu.remove()
+        numBolas--
+        num_objeto.innerHTML = numBolas
     }
 
     // vai desenhar a bolinha no DOM
     desenhar=()=>{
+        const div = document.createElement("div")
+        div.setAttribute("id",this.id)
+        div.setAttribute("class","bola")
+        div.setAttribute("style",`
+            left:${this.px}px; 
+            top:${this.py}px; 
+            width:${this.tam}px;
+            height:${this.tam}px;
+            background-color:rgb(${this.r}, ${this.g}, ${this.b});
+        `)
+        this.palco.appendChild(div)
+    }
 
+    /*
+        o controlar vai precisar do método da colisao_bordas e vai ver se encostou na borda
+
+        se a bolinha encostou na borda, ele tem que mudar a direção da posição x para -1
+    */
+
+    controle_bordas=()=>{
+        if(this.px + this.tam >= larguraPalco){
+            this.dirx=-1
+        }else if(this.px <= 0){
+            this.dirx=1
+        }
+
+        if(this.py + this.tam >= alturaPalco){
+            this.diry=-1
+        } else if (this.py <= 0){
+            this.diry = 1
+        }
+    }
+
+    // método para controlar a movimentação das bolinhas
+    // independente da posição da bolinha, ela terá que ser atualizada, então a posição cada vez que ele chama o controlar ele vai pegar a posição e atualizar paraa a nova posição
+    controlar=()=>{
+        this.controle_bordas()
+        // a posição x vai se atualizar somando a direção x multiplicado pela velocidade do x, o mesmo da posição y
+        this.px+=this.dirx*this.velx
+        this.py+=this.diry*this.vely
+        this.eu.setAttribute("style",`
+            left:${this.px}px; 
+            top:${this.py}px; 
+            width:${this.tam}px;
+            height:${this.tam}px;
+            background-color:rgb(${this.r}, ${this.g}, ${this.b});
+        `)
+        if((this.px > larguraPalco) || (this.py > alturaPalco))
+        {
+            this.remover()
+        }
     }
 }
 
@@ -95,6 +145,7 @@ btn_add.addEventListener("click",(evt)=>{
     const qtde = txt_qtde.value 
     for(let i=0; i<qtde;i++){
         // instanciar novas bolinhas
+        bolas.push(new Bola(bolas,palco))
     }
 })
 
@@ -105,5 +156,6 @@ btn_remover.addEventListener("click",(evt)=>{
     */
     bolas.map((b)=>{
         // Remover a bolinha
+        b.remover()
     })
 })
