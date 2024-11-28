bfa7a12eb07fe442e6ec69c6ee5038acb97ee40b
Ygorhenrique <118612433+YgorHenriqueLima@users.noreply.github.com>
2024-11-28 20:59:46 -0300

    fazendo uma prática utilizando os conceitos de módulo em JS

--- javascript-intermediario/modulo/cursos.js
+++ javascript-intermediario/modulo/cursos.js
@@ -1,2 +1,16 @@
-const cursos = ["javascript","Python","HTML","CSS","Raspberry","C++","Java","C#"];
-const carros = ["Polo","T-cross","golf","onix","Cruze","Argo","Cronos"];
+class Cursos{
+    static cursos = ["javascript","Python","HTML","CSS","Raspberry","C++","Java","C#"]
+    static getTodosCursos=()=>{
+        return this.cursos
+    }
+    static getCurso=(i_curso)=>{
+        return this.cursos[i_curso]
+    }
+    static addCurso=(novoCurso)=>{
+        this.cursos.push(novoCurso)
+    }
+    static apagarCursos=()=>{
+        this.cursos=[]
+    }
+}
+export default Cursos;
