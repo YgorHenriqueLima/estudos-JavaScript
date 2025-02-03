3740fffa03e74c855aea4e27f3d74e8fb2bed008
YgorHenriqueLima <118612433+YgorHenriqueLima@users.noreply.github.com>
2025-02-03 18:55:12 -0300

    salvando novas alterações deste exercicio

--- javascript-intermediario/caixa_personalizado/Cxmsg.js
+++ javascript-intermediario/caixa_personalizado/Cxmsg.js
@@ -13,7 +13,38 @@ class Cxmsg{
     }
 
     mostrar(){
-        console.log(this.texto)
+        this.divmsg = document.createElement("div");
+        /*estilo da área em que vai estar a caixinha*/
+        const estilo_divmsg=`
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            position:absolute;
+            top:0px;
+            left:0px;
+            width:100%;
+            height:100vh;
+            background-color:rgba(0,0,0,0.7);
+        `;
+
+        this.divmsg.setAttribute("id","div_msg");
+        this.divmsg.setAttribute("style",estilo_divmsg);
+        /*será adicionado ao topo*/
+        this.destino.prepend(this.divmsg)
+
+        /*essa será a caixinha e abaixo o seu estilo*/
+        const estilo_areaCxmsg =`
+            display:flex;
+            justify-content:flex-start;
+            align-items:center;
+            flex-direction:column;
+            width:300px;
+        `;
+        const areaCxmsg = document.createElement("div")
+        areaCxmsg.setAttribute("style",estilo_areaCxmsg);
+
+        this.divmsg.appendChild(areaCxmsg)
+
     }
     
     ocultar(){
